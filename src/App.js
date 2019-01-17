67e25c274f59c0b9475ae857cf2b93ff7a6a5328
Ryan McHenry <ryanmchenry2@gmail.com>
2019-01-17 17:13:33 -0600

    mobile version

--- src/App.js
+++ src/App.js
@@ -10,53 +10,147 @@ class App extends Component {
       topLeft:false,
       topRight:false,
       bottomLeft:false,
-      bottomRight:false
+      bottomRight:false,
+      width: window.innerWidth,
     }
   }
 
-  open(corner){
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  // make sure to remove the listener
+  // when the component is not mounted anymore
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
+  toggleTopLeft(){
     this.setState({
-      [corner]:!this.state.corner
+      topLeft:!this.state.topLeft,
+      topRight:false,
+      bottomLeft:false,
+      bottomRight:false
     })
   }
 
-  close(corner){
+  toggleTopRight(){
     this.setState({
-      [corner]:false
+      topLeft:false,
+      topRight:!this.state.topRight,
+      bottomLeft:false,
+      bottomRight:false
+    })
+  }
+
+  toggleBottomLeft(){
+    this.setState({
+      topLeft:false,
+      topRight:false,
+      bottomLeft:!this.state.bottomLeft,
+      bottomRight:false
+    })
+  }
+
+  toggleBottomRight(){
+    this.setState({
+      topLeft:false,
+      topRight:false,
+      bottomLeft:false,
+      bottomRight:!this.state.bottomRight
     })
   }
 
   topLeft(){
     if(this.state.topLeft){return('contentTopLeftOpen')}
-    return('contentTopLeft')
+    return('contentTopLeftMobile')
   }
 
   topRight(){
     if(this.state.topRight){return('contentTopRightOpen')}
-    return('contentTopRight')
+    return('contentTopRightMobile')
   }
 
   bottomLeft(){
     if(this.state.bottomLeft){return('contentBottomLeftOpen')}
-    return('contentBottomLeft')
+    return('contentBottomLeftMobile')
   }
 
   bottomRight(){
     if(this.state.bottomRight){return('contentBottomRightOpen')}
-    return('contentBottomRight')
+    return('contentBottomRightMobile')
   }
 
   render() {
 
-    let who="I'm a developer and product manager living in Chicago, IL. I'm graduating from Northwestern University in the Class of 2019 with a double major in Computer Science and Music Engineering."
+    const {width} = this.state;
+    const isMobile = width <= 1000;
+    if (isMobile){
+      return (
+        <div className='app'>
+          <div className='headerName'>Ryan McHenry.</div>
 
+          <div className='tagTopLeft' onClick={()=>this.toggleTopLeft()}>who i am</div>
+
+          <div className={this.topLeft()} onClick={()=>this.toggleTopLeft()}>
+            hi. my name is ryan.
+            <br/>
+            - software developer / product manager
+            <br/>
+            - living in Chicago, IL.
+            <br/>
+            - from Philadelphia, PA.
+            <br/>
+            - Northwestern University Class of 2019
+            <br/>
+            - double major in Computer Science and Music Engineering
+          </div>
+
+          <div className='tagTopRight' onClick={()=>this.toggleTopRight()}>what i do</div>
+
+          <div className={this.topRight()} onClick={()=>this.toggleTopRight()}>
+            <b>Blueprint Alpha</b>
+            <br/>
+            <div>Product Manager (January 2018 - present)</div>
+            <br/>
+            <b>Mission Measurement</b>
+            <br/>
+            <div>Product Manager (September 2018 - January 2019)</div>
+          </div>
+
+          <div className='tagBottomLeft' onClick={()=>this.toggleBottomLeft()}>what i look like</div>
+
+          <div className={this.bottomLeft()} onClick={()=>this.toggleBottomLeft()}>
+            <div className='textBottomLeft'>Heyo!</div>
+            <br/>
+            <img src='/img/headshot.jpg' className='imgBottomLeft'/>
+          </div>
+
+          <div className='tagBottomRight' onClick={()=>this.toggleBottomRight()}>contact</div>
+
+          <div className={this.bottomRight()} onClick={()=>this.toggleBottomRight()}>
+            <b>LinkedIn</b>
+            <br/>
+            <a href='https://www.linkedin.com/in/ryanmchenry2/'>@ryanmchenry2</a>
+            <br/>
+            <b>GitHub</b>
+            <br/>
+            <a href='https://www.github.com/ryanmchenry2/'>@ryanmchenry2</a>
+          </div>
+        </div>
+      );
+    }
     return (
       <div className='app'>
         <div className='headerName'>Ryan McHenry.</div>
 
-        <div className='tagTopLeft' onClick={()=>this.open('topLeft')}>who i am</div>
+        <div className='tagTopLeft'>who i am</div>
 
-        <div className={this.topLeft()} onClick={()=>this.close('topLeft')}>
+        <div className='contentTopLeft'>
           hi. my name is ryan.
           <br/>
           - software developer / product manager
@@ -70,9 +164,9 @@ class App extends Component {
           - double major in Computer Science and Music Engineering
         </div>
 
-        <div className='tagTopRight' onClick={()=>this.open('topRight')}>what i do</div>
+        <div className='tagTopRight'>what i do</div>
 
-        <div className={this.topRight()} onClick={()=>this.close('topRight')}>
+        <div className='contentTopRight'>
           <b>Blueprint Alpha</b>
           <br/>
           <div>Product Manager (January 2018 - present)</div>
@@ -82,17 +176,17 @@ class App extends Component {
           <div>Product Manager (September 2018 - January 2019)</div>
         </div>
 
-        <div className='tagBottomLeft' onClick={()=>this.open('bottomLeft')}>what i look like</div>
+        <div className='tagBottomLeft'>what i look like</div>
 
-        <div className={this.bottomLeft()} onClick={()=>this.close('bottomLeft')}>
+        <div className='contentBottomLeft'>
           <div className='textBottomLeft'>Heyo!</div>
           <br/>
           <img src='/img/headshot.jpg' className='imgBottomLeft'/>
         </div>
 
-        <div className='tagBottomRight' onClick={()=>this.open('bottomRight')}>contact</div>
+        <div className='tagBottomRight'>contact</div>
 
-        <div className={this.bottomRight()} onClick={()=>this.close('bottomRight')}>
+        <div className='contentBottomRight'>
           <b>LinkedIn</b>
           <br/>
           <a href='https://www.linkedin.com/in/ryanmchenry2/'>@ryanmchenry2</a>
