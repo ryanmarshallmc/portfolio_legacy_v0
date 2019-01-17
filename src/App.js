9e53c3b5d8d5b3e6c566a5419991f71aa07678a1
Ryan McHenry <ryanmchenry2@gmail.com>
2019-01-17 15:48:53 -0600

    big ole revamp

--- src/App.js
+++ src/App.js
@@ -1,41 +1,70 @@
-import React, { Component } from 'react';
-import Landing from './pages/Landing';
-import Who from './pages/Who';
-import What from './pages/What';
-import Where from './pages/Where';
-import Footer from './pages/Footer';
-import scrollToComponent from 'react-scroll-to-component';
-import './App.css';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './App.css'
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+
   render() {
+
+    let who="I'm a developer and product manager living in Chicago, IL. I'm graduating from Northwestern University in the Class of 2019 with a double major in Computer Science and Music Engineering."
+
     return (
-      <div className="App">
+      <div className='app'>
+        <div className='headerName'>Ryan McHenry.</div>
 
-        <div className='nav_container'>
-          <div className='nav_text_primary' onClick={()=>scrollToComponent(this.Landing, { offset: 0, align: 'top', duration: 500})}>
-            ryan mchenry.
-          </div>
+        <div className='tagTopLeft'>who i am</div>
 
-          <div className='nav_link' onClick={()=>scrollToComponent(this.Who, { offset: 0, align: 'top', duration: 500})}>
-            who i am
-          </div>
-
-          <div className='nav_link' onClick={()=>scrollToComponent(this.What, { offset: 0, align: 'top', duration: 500})}>
-            what i do
-          </div>
-
-          <div className='nav_link' onClick={()=>scrollToComponent(this.Where, { offset: 0, align: 'top', duration: 500})}>
-            where to find me
-          </div>
-
+        <div className='contentTopLeft'>
+          hi. my name is ryan.
+          <br/>
+          - software developer / product manager
+          <br/>
+          - living in Chicago, IL.
+          <br/>
+          - from Philadelphia, PA.
+          <br/>
+          - Northwestern University Class of 2019
+          <br/>
+          - double major in Computer Science and Music Engineering
         </div>
 
-        <Landing ref={(r) => { this.Landing = r; }}/>
-        <Who ref={(r) => { this.Who = r; }}/>
-        <What ref={(r) => { this.What = r; }}/>
-        <Where ref={(r) => { this.Where = r; }}/>
-        <Footer />
+        <div className='tagTopRight'>what i do</div>
+
+        <div className='contentTopRight'>
+          <b>Blueprint Alpha</b>
+          <br/>
+          <div>Product Manager (January 2018 - present)</div>
+          <br/>
+          <b>Mission Measurement</b>
+          <br/>
+          <div>Product Manager (September 2018 - January 2019)</div>
+        </div>
+
+        <div className='tagBottomLeft'>what i look like</div>
+
+        <div className='contentBottomLeft'>
+          <div className='textBottomLeft'>Heyo!</div>
+          <br/>
+          <img src='/img/headshot.jpg' className='imgBottomLeft'/>
+        </div>
+
+        <div className='tagBottomRight'>contact</div>
+
+        <div className='contentBottomRight'>
+          <b>LinkedIn</b>
+          <br/>
+          <a href='https://www.linkedin.com/in/ryanmchenry2/'>@ryanmchenry2</a>
+          <br/>
+          <b>GitHub</b>
+          <br/>
+          <a href='https://www.github.com/ryanmchenry2/'>@ryanmchenry2</a>
+        </div>
       </div>
     );
   }
