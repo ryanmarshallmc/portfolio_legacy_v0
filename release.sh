f3706a45adf06b29e3d268337e7dc0e2e68ce656
Ryan McHenry <ryanmchenry2@gmail.com>
2019-03-13 15:05:21 -0500

    adding cloudfront invalidation

--- release.sh
+++ release.sh
@@ -6,6 +6,9 @@ echo "Build complete. Optimized version ready to release."
 aws s3 cp --recursive build s3://ryan-mchenry.com --profile ryanmchenry2
 echo "sync completed - s3 is up to date."
 
+aws cloudfront create-invalidation --profile ryanmchenry2 --distribution-id E1MJG1CD11L03K --paths /
+echo "cloudfront invalidation of caches complete."
+echo ""
 read -p "Do you want to commit to GitHub?" yn
     case $yn in
         [Yy]* ) git add .
