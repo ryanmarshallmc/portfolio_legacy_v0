b649fac94ef97d9f933d4bd30c49ed8bafc1b6cd
Scott McHenry <smchenry2014@gmail.com>
2021-11-29 16:29:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
   }
 
   followMouse(e){
-    this.setState({ mouseX: e.screenX, mouseY: e.screenY - 128 });
+    this.setState({ mouseX: e.screenX, mouseY: e.screenY - 135 });
   }
 
   renderCursorLine(){
